ee22f143fd229acb4038a12b3a73d69791d2e534
vandresandes <vandresandes@gmail.com>
2018-12-19 17:13:49 -0300

    Criação do projeto

---
+++ src/main/java/br/gov/ba/pge/recaptcha/api/RecaptchaApiApplication.java
@@ -0,0 +1,14 @@
+package br.gov.ba.pge.recaptcha.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecaptchaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecaptchaApiApplication.class, args);
+	}
+
+}
+
